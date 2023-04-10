3c6bddcb426b2bcaeba993da7d101cfca5e305f2
Andrey Kovalenko <ak@taucode.com>
2023-04-10 21:36:52 +0300

    Inital commit.

---
+++ src/TauCode.Messaging.Abstractions/IMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace TauCode.Messaging.Abstractions;
+
+public interface IMessage
+{
+    string? Topic { get; set; }
+    string? CorrelationId { get; set; }
+    DateTimeOffset CreatedAt { get; set; }
+}
---
+++ test/TauCode.Messaging.Abstractions.Tests/DummyFixture.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.Messaging.Abstractions.Tests;
+
+[TestFixture]
+public class DummyFixture
+{
+    [Test]
+    public void DummyTest()
+    {
+        Assert.Pass("Added for passing Azure DevOps.");
+    }
+}
